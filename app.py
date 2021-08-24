a748f4d74a9e9f31cc2d5c7e64b4bbe114e65148
Glaucia Silva <glaucia.ap1258@gmail.com>
2021-08-24 00:35:22 -0300

    Atualização no nome do banco de dados

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/youtube'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/usuarios'
 
 db = SQLAlchemy(app)
 
